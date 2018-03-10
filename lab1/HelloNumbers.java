f16632aa5bac9e4df043498d14900e0c796141b7
quq99 <eequq99@gmail.com>
2018-03-10 16:21:52 +0800

    Lab1: get LeapYear.java and complete HelloNumbers

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,10 @@
+public class HelloNumbers{
+  public static void main(String[] args){
+	int n = 10;
+	int sum = 0;
+	for (int i=0; i<10; i++){
+	  sum = sum + i;
+	  System.out.println(sum);
+	}
+  }
+}
