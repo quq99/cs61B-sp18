43e1f87d61d391c8460dd124780570b59e91bc1d
quq99 <eequq99@gmail.com>
2018-03-10 16:36:23 +0800

    Lab1: complete LeapYear.java

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,8 +1,20 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author Qian Qu 
  */
 public class LeapYear {
 
+    /**
+     * check if a year is Leap year
+     * @param year to be analyzed
+     * @return a bool
+     */
+    public static boolean isLeapYear(int year) {
+        if (year % 400==0 || ((year % 4==0) && (year % 100!=0))) {
+            return true;
+        } else {
+            return false;
+        }
+    }
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
@@ -27,6 +39,7 @@ public class LeapYear {
             } catch (NumberFormatException e) {
                 System.out.printf("%s is not a valid number.\n", args[i]);
             }
+
         }
     }
 }
