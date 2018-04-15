38dbf3be30763b912f7f4ac0f8797dab9cf078a2
quq99 <eequq99@gmail.com>
2018-04-15 17:11:42 +0800

    Proj0: NBody.java complete readPlanets method

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -5,4 +5,24 @@ public class NBody {
         double radius = in.readDouble();
         return radius;
     }
+
+    public static Planet [] readPlanets(String filename) {
+        In in = new In(filename);
+        int numbers = in.readInt();
+        double radius = in.readDouble();
+
+        Planet [] planets = new Planet[numbers];
+        for (int i=0; i<numbers; i++) {
+            double xx = in.readDouble();
+            double yy = in.readDouble();
+            double xv = in.readDouble();
+            double yv = in.readDouble();
+            double mass = in.readDouble();
+            String name = in.readString();
+
+            planets[i] = new Planet(xx, yy, xv, yv, mass, name);
+        }
+
+        return planets;
+    }
 }
