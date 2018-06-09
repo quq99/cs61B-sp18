750d04eacb93c99b8ac9f2c79503889f7e2016f0
quq99 <eequq99@gmail.com>
2018-06-09 18:39:03 +0800

    Proj0: complete 'Create an animation' and 'Print the universe', can be submitted

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -47,5 +47,58 @@ public class NBody {
             p.draw();
         }
 
+        /**
+         * draw animation
+         */
+        StdDraw.enableDoubleBuffering();
+
+        double timeCount = 0;
+        int allPlanetsLen = allPlanets.length;
+        /**
+         * initial xForce and yForce of each planet
+         */
+        double[] xForces = new double[allPlanetsLen];
+        double[] yForces = new double[allPlanetsLen];
+        while (timeCount < T) {
+            StdDraw.clear();
+            /**
+             * initial xForce and yForce of each planet
+             */
+            for (int i=0; i<allPlanetsLen; i++) {
+                xForces[i] = allPlanets[i].calcNetForceExertedByX(allPlanets);
+                yForces[i] = allPlanets[i].calcNetForceExertedByY(allPlanets);
+            }
+            /**
+            * update on each of the planets.This 
+            * will update each planet's position, velocity, and 
+            * acceleration.
+            */
+            for (int i=0; i<allPlanetsLen; i++) {
+                allPlanets[i].update(dt, xForces[i], yForces[i]);
+            }
+
+
+            /**
+             * draw the background image
+             */
+            StdDraw.picture(0, 0, backgroundFilename);
+            /**
+             * draw all of the planets
+             */
+            for (Planet p : allPlanets) {
+                p.draw();
+            }
+            StdDraw.show();
+            StdDraw.pause(10);
+            timeCount += dt;
+        }
+        StdOut.printf("%d\n", allPlanets.length);
+        StdOut.printf("%.2e\n", radius);
+        for (int i = 0; i < allPlanets.length; i++) {
+            StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
+               allPlanets[i].xxPos, allPlanets[i].yyPos, allPlanets[i].xxVel, 
+               allPlanets[i].yyVel, allPlanets[i].mass, allPlanets[i].imgFileName);
+        }
+
     }
 }
